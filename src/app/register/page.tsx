c922f1b68172ed3d0166d085da4f87d44aeb0e92
unknown <mauijaloskor@gmail.com>
2024-03-26 10:49:39 +0600

    Create Patient Register Page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,8 +1,121 @@
+import assets from "@/assets";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+
 const RegisterPage = () => {
   return (
-    <div>
-      <h1>Register page</h1>
-    </div>
+    <Container
+      sx={{
+        padding: "50px",
+      }}
+    >
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 2,
+            p: 6,
+            textAlign: "center",
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <Image src={assets.svgs.logo} width={50} height={50} alt="logo" />
+            </Box>
+            <Box>
+              <Typography variant="h5" fontWeight={600}>
+                Patient register
+              </Typography>
+            </Box>
+          </Stack>
+
+          <Box>
+            <form>
+              <Grid container spacing={3} my={2}>
+                <Grid item md={12}>
+                  <TextField
+                    label="Name"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    label="Email"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    label="Contact No"
+                    type="tel"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    label="Address"
+                    type="text"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                fullWidth={true}
+                sx={{
+                  my: 2,
+                }}
+              >
+                Register
+              </Button>
+              <Typography component={"p"}>
+                Do you have an account ?{" "}
+                <Link href={"/login"} className="text-blue-500 underline">
+                  Login
+                </Link>
+              </Typography>
+            </form>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 
