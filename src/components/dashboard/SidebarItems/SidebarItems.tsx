735c91facf905aa70236ac3210097dec3aa2d848
unknown <mauijaloskor@gmail.com>
2024-03-30 21:46:08 +0600

    Creating A Sidebar For Menu Items

---
+++ src/components/dashboard/SidebarItems/SidebarItems.tsx
@@ -0,0 +1,79 @@
+import {
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MailIcon from "@mui/icons-material/Mail";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+
+import React from "react";
+import Image from "next/image";
+import assets from "@/assets";
+import Link from "next/link";
+
+const SidebarItems = () => {
+  const drawer = (
+    <div>
+      <Divider />
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <Box>
+      <Stack
+        direction={"row"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        gap={1}
+        py={2}
+        mt={1}
+        component={Link}
+        href="/"
+      >
+        <Image src={assets.svgs.logo} alt="logo" width={40} height={40} />
+        <Typography variant="h6" component={"h1"}>
+          PU
+          <Box component="span" color="primary.main">
+            RE
+          </Box>{" "}
+          health care
+        </Typography>
+      </Stack>
+      {drawer}
+    </Box>
+  );
+};
+
+export default SidebarItems;
