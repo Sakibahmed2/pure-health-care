a74b4a6e49ec7b10f8d2389056111ed57590d2c3
unknown <mauijaloskor@gmail.com>
2024-04-01 12:00:47 +0600

    Creating Admin Manually And Setting Up Admin Routes

---
+++ src/app/(withDashboardLayout)/dashboard/admin/doctors/page.tsx
@@ -0,0 +1,9 @@
+const DoctorsPage = () => {
+  return (
+    <div>
+      <h1>Doctor page</h1>
+    </div>
+  );
+};
+
+export default DoctorsPage;
---
+++ src/app/(withDashboardLayout)/dashboard/admin/schedules/page.tsx
@@ -0,0 +1,9 @@
+const SchedulesPage = () => {
+  return (
+    <div>
+      <h1>Schedules page</h1>
+    </div>
+  );
+};
+
+export default SchedulesPage;
